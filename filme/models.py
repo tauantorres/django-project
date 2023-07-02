85f34945764429de63727f19c6140c567b2035a9
Tauan Torres <ttmendes@stefanini.com>
2023-07-02 20:56:41 -0300

    add: adicionando novos arquivos

--- filme/models.py
+++ filme/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+LISTA_CATEGORIAS = (
+    ("ANALISE", "Análise"),
+    ("PROGRAMACAO", "Programação"),
+    ("APRESENTACAO", "Apresentação"),
+    ("OUTROS", "Outros"),
+)
+
 
 # Criar a Classe Filme
+class Filme(models.Model):
+    titulo = models.CharField(max_length=255)
+    thumb = models.ImageField(upload_to='thumb_filmes')
+    descricao = models.TextField(max_length=1000)
+    categoria = models.CharField(max_length=20, choices=LISTA_CATEGORIAS)
+    visualizacao = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField(default=timezone.now)
+
 
 # Criar a SubClasses Episodios
 
