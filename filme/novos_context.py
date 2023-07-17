60873fcb86ba7798bbe429bccae25b60e3114c40
Tauan Torres <ttmendes@stefanini.com>
2023-07-17 13:01:05 -0300

    mod

--- filme/novos_context.py
+++ filme/novos_context.py
@@ -2,9 +2,9 @@ from .models import Filme
 
 
 def lista_filmes_recentes(request):
-    lista_filmes = Filme.objects.all().order_by('-data_criacao')[:3]
+    lista_filmes = Filme.objects.all().order_by('-data_criacao')[:8]
     return {'lista_filmes_recentes': lista_filmes}
     
 def lista_filmes_em_alta(request):
-    lista_filmes = Filme.objects.all().order_by('-visualizacao')[:3]
+    lista_filmes = Filme.objects.all().order_by('-visualizacao')[:8]
     return {'lista_filmes_em_alta': lista_filmes}
